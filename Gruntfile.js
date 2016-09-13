156aa483f656331b52b3961bef06649f22f5b6fa
Jonny Maceachern <maceachern.jonny@gmail.com>
2016-09-13 20:48:59 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+	// Display elapsed time for tasks
+	require('time-grunt')(grunt);
+
+	// Load tasks
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-sass');
+
+	// Configure
+	grunt.initConfig({
+
+		// Watch
+		watch: {
+			styles: {
+				files: ['!node_modules', 'scss/**/*.scss'],
+				tasks: ['compile']
+			}
+		},
+
+		// SCSS
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'main.css':'scss/main.scss'
+				}
+			}
+		},
+
+		// Vendor prefixes
+		postcss: {
+			options: {
+				map: true,
+
+				processors: [
+					require('pixrem')(),
+					require('autoprefixer')({
+						browsers: ['last 2 versions', 'ie 8', 'ie 9']
+					}),
+					require('cssnano')() 
+				],
+			},
+			'main.min.css' : 'main.css'
+		},
+
+		browserSync: {
+			bsFiles: {
+				src: ['main.min.css', 'index.html'],
+			},
+			options: {
+				watchTask: true,
+				server: './'
+			}
+		}
+
+	});
+
+	// Register tasks
+	grunt.registerTask('default', ['browserSync', 'watch']);
+	grunt.registerTask('compile', ['sass', 'postcss']);
+
+}
